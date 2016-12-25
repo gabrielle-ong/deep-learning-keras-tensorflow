a71128f619acad9c3704aaaf6df24d6c68020a0d
GilLevi <gil.levi100@gmail.com>
2016-12-25 20:02:54 +0200

    .

---
+++ test.py
@@ -0,0 +1,34 @@
+import numpy as np
+import scipy as sp
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+import keras
+
+import numpy
+print('numpy:', numpy.__version__)
+
+import scipy
+print('scipy:', scipy.__version__)
+
+import matplotlib
+print('matplotlib:', matplotlib.__version__)
+
+import IPython
+print('iPython:', IPython.__version__)
+
+import sklearn
+print('scikit-learn:', sklearn.__version__)
+
+import keras
+print('keras: ', keras.__version__)
+
+import theano
+print('Theano: ', theano.__version__)
+
+# optional
+import tensorflow as tf
+print('Tensorflow: ', tf.__version__)
+
+
